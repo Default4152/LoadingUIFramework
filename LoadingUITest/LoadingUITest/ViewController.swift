08c1e75095990a4a1e016b7b2056b6112b0ec9f6
Conner Alegre <cga0@pm.me>
2018-09-19 14:36:11 -0600

    initial commit with framework linked

---
+++ LoadingUITest/LoadingUITest/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  LoadingUITest
+//
+//  Created by Conner on 9/19/18.
+//  Copyright © 2018 Conner. All rights reserved.
+//
+
+import UIKit
+import LoadingUI
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
